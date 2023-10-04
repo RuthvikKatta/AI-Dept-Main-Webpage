64980dbd2f99673614e7c5f85bb29a3e12861e82
RUTHVIK <katta.ruthvik7@gmail.com>
2023-10-04 23:01:22 +0530

    Changed Carousal Functionality

--- script.js
+++ script.js
@@ -1,51 +1,31 @@
 const slides = document.getElementsByClassName("mySlides");
-const dots = document.getElementsByClassName("dot");
-const deptName = document.querySelector('.dept-name');
-const hamburger = document.querySelector('.fa-bars');
-const close = document.querySelector('.fa-xmark');
-const navBar = document.querySelector('.navigation-bar');
 
 var slideIndex = 0;
-var slideIndexCurrent = 0;
-showSlides();
 
-function currentSlide(n) {
-    showSlidesCurrent(slideIndexCurrent = n);
+function showAdjacentSlides(isLeft) {
+    slideIndex = isLeft ? slideIndex - 1 : slideIndex + 1;
 
-}
+    if (slideIndex > slides.length) { 
+        slideIndex = 1 
+    } else if (slideIndex < 0) { 
+        slideIndex = slides.length - 1;
+    }
 
-function showSlidesCurrent(n) {
-    var i;
-
-    if (n > slides.length) { slideIndexCurrent = 1 }
-
-    if (n < 0) { slideIndexCurrent = slides.length }
-    for (i = 0; i < slides.length; i++) {
+    for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" current", "");
-    }
-    slides[slideIndexCurrent - 1].style.display = "block";
-    dots[slideIndexCurrent - 1].className += " current";
-    slideIndexCurrent++;
-}
 
+    slides[slideIndex].style.display = "block";
+}
 
 function showSlides() {
-    var i;
-
-    for (i = 0; i < slides.length; i++) {
+    for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" current", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " current";
 
+    slideIndex = ( slideIndex + 1 ) % slides.length;
+    slides[slideIndex].style.display = "block";
     setTimeout(showSlides, 4000);
 }
+
+showSlides();
